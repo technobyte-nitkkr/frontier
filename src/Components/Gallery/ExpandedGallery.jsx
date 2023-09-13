2072041f0abe95cd384173196ada201c1ac0edcf
Vansh Sukhija <12112021@nitkkr.ac.in>
2023-09-14 01:06:22 +0530

    added clickable section in gallery and fixed bugs

--- src/Components/Gallery/ExpandedGallery.jsx
+++ src/Components/Gallery/ExpandedGallery.jsx
@@ -1,35 +1,8 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
 import Modal from "../Modal/Modal";
-import Terminal from "../Terminal/Terminal";
 import "./ExpandedGallery.css";
 
-export default function ExpandedGallery({ onClickOutside, show, pictures }) {
-  const [events, setEvents] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentNotif, setCurrentNotif] = useState(null);
-
-  useEffect(() => {
-    axios.get("/notification").then((res) => {
-      const notifications = res.data.data.notifications;
-      let expandedObj = {};
-      notifications.forEach((notif) => {
-        expandedObj[notif.notification.title] = {
-          ...notif.notification,
-        };
-      });
-      setEvents(expandedObj);
-      setCurrentNotif({
-        title: Object.keys(expandedObj)[0],
-        ...expandedObj[Object.keys(expandedObj)[0]],
-      });
-    });
-    setIsLoading(false);
-  }, []);
-
-  console.log(pictures);
-
+export default function ExpandedGallery({ onClickOutside, show, pictures, year }) {
   return (
     <>
       <Modal
@@ -41,6 +14,7 @@ export default function ExpandedGallery({ onClickOutside, show, pictures }) {
         show={show}
         isMenuOpen={false}
         onClickOutside={onClickOutside}
+        year={year}
       />
     </>
   );
@@ -49,7 +23,7 @@ export default function ExpandedGallery({ onClickOutside, show, pictures }) {
 const GalleryImage = (pic) => {
   return (
     <div className="imageBG">
-      <img src={pic.pic} alt="" className="gallery-image"/>
+      <img src={pic.pic} alt="" className="gallery-image" />
     </div>
   );
 }
@@ -58,7 +32,7 @@ const GalleryBG = ({ pictures }) => {
   return (
     <div className="gallery-container">
       {
-        pictures.map((pic) => <GalleryImage pic={pic}/>)
+        pictures.map((pic) => <GalleryImage pic={pic} />)
       }
     </div>
   );
--- src/Components/Gallery/GalleryComponent.jsx
+++ src/Components/Gallery/GalleryComponent.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Gallery_bg from '/public/assets/Gallery/Gallery_bg_all.png'
 import './Gallery.css';
 import ExpandedGallery from './ExpandedGallery';
@@ -7,23 +7,20 @@ function GalleryComponent(item) {
     const [isOpen, setIsOpen] = useState(false);
 
     let element = item.item;
-    console.log(element);
 
     return (
         <>
-            <div className='hovertag cursor-pointer flex flex-col items-center' style={{ width: '370px', marginRight: '8rem', minWidth: '400px' }} onClick={() => {
-                setIsOpen(true);
-            }}>
+            <div className='hovertag cursor-pointer flex flex-col items-center' style={{ width: '370px', marginRight: '8rem', minWidth: '400px' }} onClick={() => setIsOpen(true)}>
                 <div className='gallerytag flex justify-center'>
                     <img className='imgbg' src={Gallery_bg} />
-                    <img className='self-center' style={{ height: '80%', position: 'absolute' }} src={element.imageURL} alt='thumbnail' />
+                    <img className='self-center' style={{ maxHeight: '80%', maxWidth: '80%', position: 'absolute' }} src={element.imageURL} alt='thumbnail' />
                 </div>
                 <h1 style={{ fontFamily: 'JetBrains Mono', fontSize: '2.5rem', marginTop: '2rem' }}>
                     {element.year}
                 </h1>
             </div>
             {isOpen && (
-                <ExpandedGallery show={isOpen} onClickOutside={() => setIsOpen(false)} pictures={element.pictures} />
+                <ExpandedGallery show={isOpen} onClickOutside={() => setIsOpen(false)} pictures={element.pictures} year={element.year} />
             )}
         </>
     )
